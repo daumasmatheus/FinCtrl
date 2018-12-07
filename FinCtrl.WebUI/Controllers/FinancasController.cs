f2d4467d1c92d99ae5544ede72ca372e3d04d0c5
Matheus Daumas <daumasmatheus@gmail.com>
2018-12-07 19:33:02 -0200

    Criado os metodos para criar, editar, deletar e visualizar detalhes do controlador Financas/ Criada a viewmodel para a entidade financa

--- FinCtrl.WebUI/Controllers/FinancasController.cs
+++ FinCtrl.WebUI/Controllers/FinancasController.cs
@@ -1,6 +1,10 @@
 ﻿using FinCtrl.Application.Interfaces.Financas;
+using FinCtrl.Application.Interfaces.Tipos;
+using FinCtrl.Domain.Entities;
+using FinCtrl.WebUI.ViewModels;
 using Microsoft.AspNet.Identity;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace FinCtrl.WebUI.Controllers
@@ -8,10 +12,13 @@ namespace FinCtrl.WebUI.Controllers
     public class FinancasController : Controller
     {
         private readonly IFinancasServices _financasServices;
+        private readonly ITiposService _tiposService;
 
-        public FinancasController(IFinancasServices financasServices)
+        public FinancasController(IFinancasServices financasServices,
+                                  ITiposService tiposService)
         {
             _financasServices = financasServices;
+            _tiposService = tiposService;
         }
 
         [HttpGet]
@@ -25,11 +32,87 @@ namespace FinCtrl.WebUI.Controllers
         }
 
         [HttpGet]
+        public ActionResult Details(string Id)
+        {
+            var financasDetail = _financasServices.Find(Id);
+
+            return PartialView(financasDetail);
+        }
+
+        [HttpGet]
         public ActionResult AddFinanca()
         {
-
+            ViewBag.tipos = new SelectList(_tiposService.GetTipos(), "Id", "Nome");
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddFinanca(FinancaViewModel financaViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Financa newFinanca = new Financa()
+                {
+                    Nome = financaViewModel.Nome,
+                    DataEntrada = financaViewModel.DataEntrada,
+                    Observacao = financaViewModel.Observacao,
+                    TipoId = financaViewModel.TipoId,
+                    Valor = financaViewModel.Valor,
+                    UserId = User.Identity.GetUserId()
+                };
+
+                _financasServices.Add(newFinanca);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        [HttpGet]
+        public ActionResult EditFinanca(string financaId)
+        {
+            ViewBag.tipos = new SelectList(_tiposService.GetTipos(), "Id", "Nome");
+
+            var userId = User.Identity.GetUserId();
+
+            var financaToEdit = _financasServices.Find(x => x.Id == financaId && x.UserId == userId);
+
+            return View(financaToEdit);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditFinanca(FinancaViewModel financaViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Financa financaToEdit = new Financa()
+                {
+                    Nome = financaViewModel.Nome,
+                    DataEntrada = financaViewModel.DataEntrada,
+                    Observacao = financaViewModel.Observacao,
+                    TipoId = financaViewModel.TipoId,
+                    Valor = financaViewModel.Valor
+                };
+
+                _financasServices.Edit(financaToEdit);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteFinanca(string financaId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var financaToDelete = _financasServices.Find(x => x.Id == financaId && x.UserId == userId);
+
+            return View(financaToDelete);
         }
     }
 }
---
+++ FinCtrl.WebUI/ViewModels/FinancaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinCtrl.WebUI.ViewModels
+{
+    public class FinancaViewModel
+    {
+        [Required(ErrorMessage = "Informe o titulo da finança.")]
+        [MaxLength(255, ErrorMessage = "Limite de 255 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a data da entrada do registro.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataEntrada { get; set; }
+
+        [MaxLength(25555, ErrorMessage = "Limite de 25.555 caracteres.")]
+        public string Observacao { get; set; }
+
+        [Required(ErrorMessage = "Informe o valor.")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Informe o tipo da financa: Despesa ou Rendimento.")]
+        public int TipoId { get; set; }
+    }
+}
