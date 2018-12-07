c3fef283a8ae5dbcb96f0eadfb8fa30109389fb1
Matheus Daumas <daumasmatheus@gmail.com>
2018-12-07 18:58:08 -0200

    Criado o controller Financa / Adicionado a actionresult index

---
+++ FinCtrl.WebUI/Controllers/FinancasController.cs
@@ -0,0 +1,27 @@
+﻿using FinCtrl.Application.Interfaces.Financas;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FinCtrl.WebUI.Controllers
+{
+    public class FinancasController : Controller
+    {
+        private readonly IFinancasServices _financasServices;
+
+        public FinancasController(IFinancasServices financasServices)
+        {
+            _financasServices = financasServices;
+        }
+
+        // GET: Financas
+        public ActionResult Index()
+        {
+            var loggedUserId = User.Identity.GetUserId();
+
+            var result = _financasServices.GetFinancas().Where(x => x.UserId == loggedUserId);
+
+            return View(result);
+        }
+    }
+}
